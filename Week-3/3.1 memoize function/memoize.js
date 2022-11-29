43cfa6c41ca0e7790ffaf045cf8ec6fbe5081dee
omdubey <omdubey1021@gmail.com>
2022-11-29 15:28:14 +0530

    created memoize function from scratch

---
+++ Week-3/3.1 memoize function/memoize.js
@@ -0,0 +1,45 @@
+/* Memoization: Memoization is a technique for speeding up applications
+ by caching the results of expensive function calls and returning 
+ them when the same inputs are used again. */
+
+//reducer function (factorial of a number)
+
+function fact(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return n * fact(n - 1);
+}
+
+// memoization
+
+function memoize(fn) {
+  const cache = new Map();
+  return function (...args) {
+    const key = args.toString();
+    if (cache.has(key)) return cache.get(key);
+    cache.set(key, fn(...args));
+    return cache.get(key);
+  };
+}
+
+// time function
+
+function time(fn) {
+  console.time();
+  fn();
+  console.log(fn());
+  console.timeEnd();
+}
+
+// execution
+
+const factm = memoize(fact);
+
+time(() => factm(100));
+time(() => factm(100));
+time(() => factm(100));
+
+time(() => factm(20));
+time(() => factm(20));
+time(() => factm(20));
